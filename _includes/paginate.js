8034610deb7e18968718e07ac76bfb73f7f63933
One <onewesh@gmail.com>
2018-02-22 10:05:42 -0500

    add ajax pagination code

---
+++ _includes/paginate.js
@@ -0,0 +1,28 @@
+(function loadPosts(){
+  
+  function loadMorePosts() {
+    var _this = this;
+    var $blogContainer = $(".post-list");
+    var nextPage = parseInt($blogContainer.attr("data-page")) + 1;
+    var totalPages = parseInt($blogContainer.attr("data-totalPages"));
+    
+    $(this).addClass("loading");
+    console.log(nextPage);
+    $.get("/page" + nextPage, function (data) {
+      var htmlData = $.parseHTML(data);
+      var $articles = $(htmlData).find(".post-list");
+      
+      $blogContainer.attr("data-page", nextPage).append($articles);
+      
+      if ($blogContainer.attr("data-totalPages") == nextPage) {
+        $('.loadMore').disabled = true;
+        $(".loadMore").remove();
+      }
+      
+      $(_this).removeClass("loading");
+    });  
+
+  }
+
+  $(".loadMore").click(loadMorePosts);
+})();
